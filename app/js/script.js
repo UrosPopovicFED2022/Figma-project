db0c86f8c20af9e05365479fdc874027baa14a44
Uros Popovic <uros32popovic@gmail.com>
2022-10-28 17:41:06 +0200

    almost finished tablet version, just section brand remains to fix cards

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,28 @@
 "use strict";
 
+const body = document.querySelector("body");
 const menuIcon = document.querySelector(".header__icon--open");
 const menuClose = document.querySelector(".header__icon--close");
 const navigation = document.querySelector(".header__nav");
 
-menuIcon.addEventListener("click", function () {
+const menuIconFunctionality = function () {
+  menuIcon.style.display = "none";
+  menuClose.style.display = "block";
   navigation.classList.toggle("active");
+  body.classList.toggle("overflow-active");
+};
+
+const closeIconFunctionality = function () {
+  menuIcon.style.display = "block";
+  menuClose.style.display = "none";
+  navigation.classList.toggle("active");
+  body.classList.toggle("overflow-active");
+};
+
+menuIcon.addEventListener("click", function () {
+  menuIconFunctionality();
+});
+
+menuClose.addEventListener("click", function () {
+  closeIconFunctionality();
 });
